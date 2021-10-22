69397c7e62ba5f1825260040c942e4c06dbc5ee0
rubenbimmel <ruben.bimmel@gmail.com>
2021-10-22 10:25:40 +0200

    Add example scene

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1256387.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 58309902, 58309903, 58309904, 58309905 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: false,
+    useMouse: false,
+    useGamepads: false,
+    useTouch: false
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/58309926/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/58309927/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/58309925/1/ammo.js', 'preload' : true},
+];
